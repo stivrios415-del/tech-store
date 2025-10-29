235bfc17329fb45cb75f5f2957ce3dd46d411b6b
Stiv <stivrios415@gmail.com>
2025-10-29 12:20:31 -0600

    feat: nuevo logo SVG + limpieza de archivos obsoletos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import logo from '../logoqq.svg';
+import logo from './logo.svg';
 
 const Header = ({ cartItemsCount, onCartClick }) => {
   return (
